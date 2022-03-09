08e5837e92920720142f9ece72aca2c89ee93e9a
nicolastanguy.bouquiaux@student.kdg.be </home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/Black_Bishop.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/Black_King.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/Black_Knight.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/Black_Pawn.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/Black_Queen.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/Black_Rook.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/clash.jpg /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/icon.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/White_Bishop.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/White_King.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/White_Knight.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/White_Pawn.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/White_Queen.png /home/nicolas/Downloads/ChessOOP-master/Chess/bin/chess/White_Rook.png>
2022-03-09 01:50:46 +0100

    cleaned up code

--- src/GameApplication/view/board/components/ChessBoard.java
+++ src/GameApplication/view/board/components/ChessBoard.java
@@ -18,13 +18,15 @@ public class ChessBoard extends GridPane {
         pieceComponentsOfBoard = new PieceComp[8][8];
         piecesFromBoard = new Piece[8][8];
 
-        /*
-        for (int x = 0; x < spaces[0].length; x++) {
-            for (int y = 0; y < spaces[1].length; y++) {
+        //kolom
+        for (int y = 7; y >= 0; y--) {
+            //rij
+            for (int x = 0; x < 8; x++) {
                 boolean light = ((x + y) % 2 != 0);
                 spaces[x][y] = new Space(light, x, y);
 
-                if (playerIsWhite) {
+                if (!playerIsWhite) {
+                    System.out.println(x + " " + y);
                     this.add(spaces[x][y], x, 7 - y);
                 } else {
                     this.add(spaces[x][y], 7 - x, y);
@@ -32,36 +34,8 @@ public class ChessBoard extends GridPane {
                 final int xVal = x;
                 final int yVal = y;
                 spaces[x][y].setOnAction(e -> onSpaceClick(xVal, yVal));
-
-
             }
         }
-
-         */
-
-        //kolom
-        for (int y = 7; y >=0; y--) {
-            //rij
-            for (int x = 0; x <8; x++) {
-                boolean light = ((x + y) % 2 != 0);
-                spaces[x][y] = new Space(light, x, y);
-
-                if (!playerIsWhite) {
-                    System.out.println(x + " " +y);
-                    this.add(spaces[x][y], x, 7- y);
-
-                } else {
-                    this.add(spaces[x][y], 7 -x, y);
-
-                }
-                final int xVal = x;
-                final int yVal = y;
-                spaces[x][y].setOnAction(e -> onSpaceClick(xVal, yVal));
-
-
-            }
-        }
-       // this.defineStartPositions();
     }
 
     public void setActiveSpace(Space s) {
@@ -76,7 +50,7 @@ public class ChessBoard extends GridPane {
             this.activeSpace.getStyleClass().add("chess-space-active");
     }
 
-    public void onSpaceClickV2(Board board, int clickedColumn, int clickedRow){
+    public void onSpaceClickV2(Board board, int clickedColumn, int clickedRow) {
         board.getMoveManager().addMove(clickedColumn, clickedRow);
 
         System.out.println(board.getMoveManager().getMoves());
@@ -193,71 +167,20 @@ public class ChessBoard extends GridPane {
     }
 
 
-
-
     public void defineStartPositions(Piece[][] piecesFromBoard) {
-      //  System.out.println("Pieces from model: "+piecesFromBoard[0][0].getPieceLocation());
         this.piecesFromBoard = piecesFromBoard;
 
-
-        /*
         for (int i = 7; i >= 0; i--) {
             for (int j = 0; j < 8; j++) {
-                this.spaces[j][i].setPiece(null);
-                if (this.piecesFromBoard[j][i]!=null) {
-                    pieceComponentsOfBoard[j][i] = PieceComp.fromPieceToPieceComp(this.piecesFromBoard[j][i]);
-                    this.spaces[j][i].setPiece(pieceComponentsOfBoard[j][i]);
-                    //System.out.println(i + " " + j);
-                }
-            }
-        }
-
-         */
-
-        for (int i = 7; i >= 0; i--) {
-            for (int j = 0; j < 8; j++) {
-                this.spaces[7-j][i].setPiece(null);
-                if (this.piecesFromBoard[7-j][i]!=null) {
-                    pieceComponentsOfBoard[7-j][i] = PieceComp.fromPieceToPieceComp(this.piecesFromBoard[7-j][i]);
-                    this.spaces[7-j][i].setPiece(pieceComponentsOfBoard[7-j][i]);
-                    //.out.println((i )+ " " + (7-j)+" "+pieceComponentsOfBoard[7-j][i].getName()+" "+pieceComponentsOfBoard[7-j][i].getColor());
-                    //System.out.println(i + " " + j);
+                this.spaces[7 - j][i].setPiece(null);
+                if (this.piecesFromBoard[7 - j][i] != null) {
+                    pieceComponentsOfBoard[7 - j][i] = PieceComp.fromPieceToPieceComp(this.piecesFromBoard[7 - j][i]);
+                    this.spaces[7 - j][i].setPiece(pieceComponentsOfBoard[7 - j][i]);
                 }
 
 
             }
         }
-
-
-        /*
-        // white pieces
-        this.spaces[0][0].setPiece(new Rook(true));
-        this.spaces[1][0].setPiece(new Knight(true));
-        this.spaces[2][0].setPiece(new Bishop(true));
-        this.spaces[3][0].setPiece(new Queen(true));
-        this.spaces[4][0].setPiece(new King(true));
-        this.spaces[5][0].setPiece(new Bishop(true));
-        this.spaces[6][0].setPiece(new Knight(true));
-        this.spaces[7][0].setPiece(new Rook(true));
-
-        for (int i = 0; i < this.spaces[0].length; i++)
-            this.spaces[i][1].setPiece(new Pawn(true));
-
-        // black pieces
-        this.spaces[0][7].setPiece(new Rook(false));
-        this.spaces[1][7].setPiece(new Knight(false));
-        this.spaces[2][7].setPiece(new Bishop(false));
-        this.spaces[3][7].setPiece(new Queen(false));
-        this.spaces[4][7].setPiece(new King(false));
-        this.spaces[5][7].setPiece(new Bishop(false));
-        this.spaces[6][7].setPiece(new Knight(false));
-        this.spaces[7][7].setPiece(new Rook(false));
-
-        for (int i = 0; i < this.spaces[0].length; i++)
-            this.spaces[i][6].setPiece(new Pawn(false));
-
-
-         */
     }
 
 }
